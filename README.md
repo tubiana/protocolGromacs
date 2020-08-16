443d3beb55f365943700e972bf28f5d12d285772
Thibault Tubiana <tubiana.thibault@gmail.com>
2020-08-16 21:05:14 +0200

    Initial commit - MD protocol version 1.3

---
+++ README.md
@@ -0,0 +1,76 @@
+# Automatic Gromacs Workflow Script
+*Author: Thibault Tubiana, Ph.D.*
+**Please read before using this script.**
+
+## Description
+This script is made to facilitate the preparation and production of protein and protein/ligand MD.  
+It follows the procedure described for a teaching I made at the University of Bergen. You can find lectures content on this page http://tubiana.me/teaching/kjem220-molecular-modelling/ or the pdf describing all the steps on this script here: http://tubiana.me/teaching_files/biocat2020/Tutorial_Gromacs-2019.pdf 
+Basic analysis are also generated with gromacs tools (temperature/pressure/rmsd/rmsf......) and the production trajectories are also cleaned with trjconv (imaging/protein centered/water stripped), but all the original trajectories are kept.  
+Feel free to make other analysis of course, like trajectory clustering with ttclust https://github.com/tubiana/TTClust 😇
+
+## Disclamer
+* Each system is unique. This protocol and MD parameters is not adapted for all systems. If your system crash, you may have to tweak MDP parameters.  
+* Ligand parametrisation is *"quick and dirty"*, For a more stable MD system you may have to tweak acpype parameters (and check the hydrogens that are added with babel).
+
+
+## Dependancies
+- this script only work on linux (maybe mac) and use the BASH syntax.
+- For ligand parametrization I use ACPYPE (https://github.com/alanwilter/acpype) which can generate parameters for Amber, Gromacs and Charmm. Please cite this paper if you use ACPYPE: https://doi.org/10.1016/j.softx.2019.100241.  
+   1. To install acpype, I sugg0est you to install first miniconda (if you don't already have conda https://docs.conda.io/en/latest/miniconda.html) and the create a new conda environement with the command `conda create -n acpype -c conda-forge acpype` 
+   2. then activate the environment with `conda activate acpype`
+
+## How to
+1. Make sure you have all the dependancies 
+    1. If you have a protein-ligand system, make sur acpype is installed (see **parameters**)
+    2. Gromacs
+    3. (optional) DSSP version 3
+2. Clone this repository with the command `git clone https://github.com/tubiana/protocolGromacs.git`
+3. Put your PDB in the repository
+4. Make the change you need in runGromacs.sh (See **parameters**)
+5. run the script with `bash runGromacs.sh`
+
+## Parameters
+You have to make some changes in the script file (runGromacs).
+- **FILE**: PDB filename **without the extension** (2h4g.pdb --> FILE=2h4g)
+- **LIGNAME**: 3 letter ligand name (it has to be the same in the PDB). NOTE: **The ligand name will be change to `LIG` afterward.**
+- **BOXSIZE**: Periodic box size in nm (between protein and box facet) default is **1.2**
+- **BOXTYPE**: Box type. Default is **cubic** (see http://manual.gromacs.org/documentation/5.1.4/onlinehelp/gmx-editconf.html for more details)
+- **NT**: Number of CPU cores. Default is **8**
+- **WATER**: Water type. Default is **tip3p**
+- **NUMBEROFREPLICAS**: Number of replicas (same simulation will be done 3 times from the minimisation). Default is **3**
+- **FF**: Force field, default is **amber99sb-ildn**
+- **SIMULATIONTIME**: Simulation time in `ns`. Default is **100**. The script will automatically calculate and modify the number of steep according the timestep in mdp/md_prod.mpd.
+
+## Workflow
+
+Here's a picture describing the workflow in this script, but you can find more information on each steps on my tutorial http://tubiana.me/teaching_files/biocat2020/Tutorial_Gromacs-2019.pdf. You can of course modify my script as you want :-)
+
+![](img/gromacs_protocol.png "gromacs_protocol" )
+
+## folder structure
+Here's a description of the folder structure after a simulation job:
+```
+|-- .                             #--> repo folder, the script, the initial structure and topologie files
+    |-- param                     #--> only if ligand is present, will contain receptor and ligand parameters
+        |-- receptor              #--> receptor structure and topology
+        |-- ligand                #--> receptor topology
+            |-- ligand.acpype     #--> ligand topology
+    |-- mdp                       #--> original mdp parameters
+    |-- replica_X                 #--> simulation for replica number X (if 3 replica, then 3 folders)
+        |-- graph                 #--> All the output graph are saved here (rmsd,rmsf,energy.....)
+        |-- gro                   #--> Some output structures from MD are saved here
+        |-- mdp                   #--> copy of previous mdp folder
+        |-- results               #--> contains the MD
+            |-- mini              #--> minimisation MD files
+            |-- nvt               #--> heationg MD files
+            |-- npt               #--> equilibration MD files
+            |-- prod              #--> production MD files
+```
+
+
+## Last thing...
+Have fun with MD and send me a mail if or open an issue if you have any issues, or just if you used this script and want to thanks me, I will be please to know that it was useful for someone 🙂
+
+
+
+Thibault Tubiana.
---
+++ runGromacs.sh
@@ -0,0 +1,297 @@
+#!/bin/bash
+#Author: Thibault Tubiana, PhD, 2020
+#Version: 1.3.0
+
+#activate acpype environment
+#conda activate acpype
+
+#---------  FILE SETUP  -----------
+#PDB FILE, will be replace in the code later by $PDB
+FILE="" #<<<<<<<<<<<<<<<<<<<<<<<<< PUT THE PDB NAME HERE (without the extension)
+# LIGAND NAME, if you have a ligand, it will be parametrize with acpype and the ligand name will be replace by "LIG".
+LIGNAME="" #<<<<<<<<<<<<<<<<<<<<<<  #PUT LIGAND NAME HERE, leave it blank if no ligand.
+
+#---------  SIMU SETUP  -----------
+BOXSIZE=1.2 #cubic simulation boxsiwe
+BOXTYPE=cubic #Box type
+NT=8 #Number of core.
+WATER=tip3p #Water type
+NUMBEROFREPLICAS=3 #Number of replica
+FF=amber99sb-ildn #Force field
+SIMULATIONTIME=100 #Simulation time in nanosec. Will be converted in fs and modified in the mdp file.
+
+#---------  HPC SETUP  -----------
+MPI="" #If you have to submit jobs with MPI softwares like "mpirun -np 10". Add the command here
+GMX=gmx #GMX command (can be "$GMX_mpi" sometimes. Just change it here
+
+if [ ! -z "$LIGNAME" ]
+then
+      #Create parameters directories
+      mkdir param
+      cp $FILE".pdb" param/
+      cd param
+
+      grep 'ATOM  ' $FILE".pdb" --color=none > receptor.pdb
+
+      #Extract ligand and connect
+      python_command=$(python <<EOF
+ligand_atom = []
+keepLine = []
+with open("$FILE.pdb","r") as file:
+    lines = file.readlines()
+    for line in lines:
+        if '$LIGNAME' in line[17:20]:
+            line = line[:17]+"LIG"+line[20:]
+            keepLine.append(line)
+            ligand_atom.append(int(line[6:11]))
+        elif "CONECT" in line[0:6]:
+            idx = [int(x) for x in line.split()[1:]]
+            if any(id in idx for id in ligand_atom):
+                keepLine.append(line)
+with open("ligand.pdb","w") as file:
+    for line in keepLine:
+        file.write(line)
+EOF
+)
+
+
+    #Convert in mol2 while adding hydrogens.
+    obabel -ipdb ligand.pdb -omol2 -h > ligand.mol2
+
+    #use ACPYPE to create the ligand topology.
+    #DISCLAMER! This is a "quick and dirty method", it has to be optimised with ACPYPE parameters of course and adapted to ligands
+    #if you see strange MD behaviours.
+    acpype -i ligand.mol2
+    mkdir ligand
+    mv ligand* ligand/
+    mkdir receptor
+    mv receptor.pdb receptor/
+    cd receptor
+
+    #Preparing topology
+    $GMX pdb2$GMX -f receptor.pdb -o receptor_GMX.pdb -water $WATER -ignh -ff $FF
+
+    #Copy files from receptors/ligand folders.
+    cd ../../
+    cp param/receptor/*.itp param/receptor/topol.top .
+    cp param/ligand/ligand.acpype/ligand_GMX.itp ligand.itp
+    grep -h ATOM param/receptor/receptor_GMX.pdb param/ligand/ligand.acpype/ligand_NEW.pdb > complex.pdb
+
+    #Add the ligand topology inside "topol.top"
+    cp topol.top topol.bak
+    cat topol.top | sed '/forcefield\.itp\"/a\
+  #include "ligand.itp"
+  ' > topol2.top
+    mv topol2.top topol.top
+    echo "ligand   1" >> topol.top
+
+#Generate idx group for ligand without hydrogens (for restraints)
+ndx=$($GMX make_ndx -f param/ligand/ligand.acpype/ligand_NEW.pdb -o lig_noh.ndx <<EOF
+r LIG & !a H*
+name 3 LIG-H
+q
+EOF
+)	
+    echo "LIG-H" | $GMX genrestr -f param/ligand/ligand.acpype/ligand_NEW.pdb -o posre_ligand.itp -n lig_noh.ndx -fc 1000 1000 1000
+	
+	#Include posre_ligand.itp AT THE END!!!!!!! of  ligand.itp
+	echo '
+	
+	 ; Include Position restraint file
+#ifdef POSRES
+#include "posre_ligand.itp"
+#endif'  >> ligand.itp
+
+    $GMX editconf -f  complex.pdb -o complex_newbox.gro -d $BOXSIZE -bt $BOXTYPE
+	PDB=complex
+else
+
+	#set "PDB" name (all the simulation filenames are based on this variable).
+	PDB=$FILE
+	########################
+	##   TOPOLOGIE Creation
+	#######################
+	$GMX pdb2GMX -f $PDB".pdb" -o $PDB"_processed.gro" -water $WATER -ignh -ff $FF
+	########################
+	##   Solvatation
+	#######################
+	#Default editconf, changer if you want...
+	$GMX editconf -f  $PDB"_processed.gro" -o $PDB"_newbox.gro" -d $BOXSIZE -bt $BOXTYPE
+fi
+
+
+
+#SOLVATATION
+$GMX solvate -cp $PDB"_newbox.gro" -cs spc216.gro -o $PDB"_solv.gro" -p topol.top
+
+#######################
+## ADDING IONS
+#######################
+$GMX grompp -f mdp/ions.mdp -c $PDB"_solv.gro" -p topol.top -o ions.tpr --maxwarn 1
+
+echo "SOL" | $GMX genion -s ions.tpr -o $PDB"_solv_ions.gro" -p topol.top -pname NA -nname CL -neutral
+
+
+ 
+for ((i=0; i<$NUMBEROFREPLICAS; i++))
+	do 
+	echo ">>>>> replica_"$((i+1))
+	mkdir "replica_"$((i+1))
+	cd "replica_"$((i+1))
+	cp -R ../mdp .
+	cp ../$PDB"_solv_ions.gro" .
+	cp ../topol.top .
+	cp ../*.itp .
+
+
+	#######################
+	## MINIMISATION
+	#######################
+	$GMX grompp -f mdp/em.mdp -c $PDB"_solv_ions.gro" -p topol.top -o em.tpr
+	$MPI $GMX mdrun -v -deffnm em -nt $NT
+
+
+	#Cleaning
+	mkdir -p results/mini
+	mkdir -p gro
+	mkdir graph
+	mv em* results/mini/
+	mv mdout.mdp results/mini/
+	mv *.gro gro/
+	#potential energy graph
+	echo "11 0" | $GMX energy -f results/mini/em.edr -o graph/mini_"$PDB"_pot.xvg
+
+
+
+
+
+	#######################
+	## temperature 300
+	#######################
+	$GMX grompp -f mdp/nvt_300.mdp -c results/mini/em.gro -r results/mini/em.gro  -p topol.top -o nvt_300.tpr -maxwarn 2
+	$MPI $GMX mdrun -deffnm nvt_300 -v -nt $NT
+	#temperature_graph
+
+	#cleaning
+	mkdir -p results/nvt
+	mv nvt* results/nvt/ 2> /dev/null
+	mv mdout.mdp results/nvt/
+
+	#Temparture graph
+	echo "16 0" | $GMX energy -f results/nvt/nvt_300.edr -o graph/temperature_nvt_300.xvg
+
+	#######################
+	## Pression
+	#######################
+	$GMX grompp -f mdp/npt.mdp -c results/nvt/nvt_300.gro -r results/nvt/nvt_300.gro -t results/nvt/nvt_300.cpt -p topol.top -o npt_ab.tpr -maxwarn 2
+	$MPI $GMX mdrun -deffnm npt_ab -v -nt $NT
+
+	#cleaning
+	mkdir -p results/npt
+	mv npt* results/npt/ 2> /dev/null
+	mv mdout.mdp results/npt_ab/
+	#Pression and density graph
+	echo "17 0" | $GMX energy -f results/npt/npt_ab.edr -o graph/npt_"$PDB"_pressure.xvg
+	echo "22 0" | $GMX energy -f results/npt/npt_ab.edr -o graph/npt_"$PDB"_volume.xvg
+
+
+
+	#######################
+	## Production 
+	#######################
+	
+	#Setup simulationtime
+if [ ! -z "$SIMULATIONTIME" ]
+then
+python_command=$(python <<EOF
+import re
+restep = re.compile("nsteps *= *(\d*)")
+redt = re.compile("dt *= *(\d*.\d*)")
+dt = 0
+simulationtime = float($SIMULATIONTIME) *1000 #Time in ps
+outputLines = []
+with open("mdp/md_prod.mdp",'r') as f:
+    mdp = f.readlines()
+    #find first the timestep
+    for line in mdp: 
+        dtmatch = redt.match(line)
+        if dtmatch:
+            dt = float(dtmatch.group(1))
+            break
+    for line in mdp:
+        stepmatch = restep.match(line)
+        if stepmatch and float(dt) > 0:
+            nsteps = int(simulationtime)/dt
+            line = "nsteps            = {}        ; {} * {} = {} ps or {} ns\n".format(int(nsteps),dt,nsteps,nsteps, dt*nsteps, simulationtime/1000)
+        outputLines.append(line)
+    with open("mdp/md_prod.mdp",'w') as f:
+        for line in outputLines:
+            f.write(line)
+EOF
+)          
+fi
+	
+	$GMX grompp -f mdp/md_prod.mdp -c results/npt/npt_ab.gro -t results/npt/npt_ab.cpt -p topol.top -o "md_"$PDB"_prod.tpr" -maxwarn 2
+	$MPI $GMX mdrun -deffnm "md_"$PDB"_prod"  -v -nt $NT
+
+	mkdir -p results/prod
+	mv md_* results/prod 2> /dev/null
+	mv mdout.mdp results/prod/
+
+	echo "backbone backbone" | $GMX rms -s "results/prod/md_"$PDB"_prod.tpr" -f "results/prod/md_"$PDB"_prod.trr" -o graph/prod_"$PDB"_rmsd.xvg -tu ns
+
+	cd results/prod
+
+	echo "Protein System" | $GMX trjconv -s "md_"$PDB"_prod.tpr" -f "md_"$PDB"_prod.trr" -o "md_"$PDB"_clean_temp.xtc" -pbc nojump -ur compact -center
+
+	echo "Protein System" | $GMX trjconv -s "md_"$PDB"_prod.tpr" -f "md_"$PDB"_clean_temp.xtc" -o "md_"$PDB"_clean_full.xtc" -fit rot+trans
+
+	echo "Protein non-Water" | $GMX trjconv -s "md_"$PDB"_prod.tpr" -f "md_"$PDB"_clean_temp.xtc" -o "md_"$PDB"_clean_nowat.xtc" -fit rot+trans
+
+	echo "Protein non-Water" | $GMX trjconv -s "md_"$PDB"_prod.tpr" -f "md_"$PDB"_clean_temp.xtc" -o "md_"$PDB"_clean_nowat.pdb" -pbc nojump -ur compact -center -b 0 -e 0
+
+	rm "md_"$PDB"_clean_temp.pdb"
+	
+	echo "non-Water" | $GMX convert-tpr -s "md_"$PDB"_prod.tpr" -o tpr_nowat.tpr
+
+	cd ../../
+	#Final graph
+	echo "backbone backbone" | $GMX rms -s "results/prod/tpr_nowat.tpr" -f "results/prod/md_"$PDB"_clean_nowat.xtc" -o "graph/prod_"$PDB"_rmsd_ca.xvg" -tu ns
+	echo "protein protein" | $GMX rms -s "results/prod/tpr_nowat.tpr" -f "results/prod/md_"$PDB"_clean_nowat.xtc" -o "graph/prod_"$PDB"_rmsd_all.xvg" -tu ns
+	echo "backbone" | $GMX gyrate -s "results/prod/tpr_nowat.tpr" -f "results/prod/md_"$PDB"_clean_nowat.xtc" -o "graph/prod_"$PDB"_gyrate.xvg"
+
+	echo "backbone" | $GMX rmsf -s "results/prod/tpr_nowat.tpr" -f "results/prod/md_"$PDB"_clean_nowat.xtc" -o "graph/prod_"$PDB"_rmsf_ref.xvg" -res
+ls
+	#####
+	# TESTING DSSP Installation
+	#####
+	export DSSP=`which dssp`
+	if [ -z "$DSSP" ]
+	then
+	      export DSSP=`which mkdssp`
+	else
+		echo "DSSP found. Good!"
+	fi
+
+	if [ -z "$DSSP" ]
+	then
+	      echo "DSSP SOFTWARE NOT FOUND. If you have anaconda or miniconda install, please install it with this command line:"
+		  echo "conda install -c conda-forge -c salilab dssp"
+		  echo "note that the name will be mkdssp"
+		  echo "Trying to install it right now..."
+		  conda install -y -c conda-forge -c salilab dssp
+		  export DSSP=`which mkdssp`
+		if [ -z "$DSSP" ]
+			then
+			      echo "Installation faillure..."
+			else
+				echo "1" |  $GMX do_dssp -s "results/prod/tpr_nowat.tpr" -f "results/prod/md_"$PDB"_clean_nowat.xtc" -o "graph/prod_"$PDB"_ss.xpm" -tu ns -dt 0.05 -ver 3
+				$GMX xpm2ps -f "graph/prod_"$PDB"_ss.xpm" -o "graph/prod_"$PDB"_ss.ps" -by 10 -bx 3
+			fi
+	else
+		echo "Protein" | $GMX do_dssp -s "results/prod/tpr_nowat.tpr" -f "results/prod/md_"$PDB"_clean_nowat.xtc" -o "graph/prod_"$PDB"_ss.xpm" -ver 3 -tu ns -dt 0.05
+		$GMX xpm2ps -f "graph/prod_"$PDB"_ss.xpm" -o "graph/prod_"$PDB"_ss.ps" -by 10 -bx 3
+	fi
+
+	cd ../
+done
