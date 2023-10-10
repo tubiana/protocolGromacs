79364148e7dd92da55e9243652edc6e7f289c425
Thibault Tubiana <tubiana.thibault@gmail.com>
2023-10-10 21:40:48 +0200

    Improving simu with Ligand and GPU

--- runGromacs.sh
+++ runGromacs.sh
@@ -12,7 +12,7 @@ FILE="" #<<<<<<<<<<<<<<<<<<<<<<<<< PUT THE PDB NAME HERE (without the extension)
 LIGNAME="" #<<<<<<<<<<<<<<<<<<<<<<  #PUT LIGAND NAME HERE, leave it blank if no ligand.
 
 #---------  SIMU SETUP  -----------
-BOXSIZE=1.5 #cubic simulation boxsiwe
+BOXSIZE=3 #cubic simulation boxsiwe
 BOXTYPE=cubic #Box type
 NT=20 #Number of core.
 WATER=tip3p #Water type
@@ -20,14 +20,52 @@ NUMBEROFREPLICAS=1 #Number of replica
 FF=amber99sb-ildn #Force field
 SIMULATIONTIME=500 #Simulation time in nanosec. Will be converted in fs and modified in the mdp file.
 
+
 #---------  HPC SETUP  -----------
 MPI="" #If you have to submit jobs with MPI softwares like "mpirun -np 10". Add the command here
 GMX=gmx #GMX command (can be "$GMX_mpi" sometimes. Just change it here
 #THOSE COMMANDS 
-GPU0="-gpu_id 0 -ntmpi 4 -ntomp 2 -nb gpu -bonded gpu -nmpe 1 -pme gpu -pmefft gpu -pin on -pinstride 0 -nstlist 100 -pinoffset 49" 
-GPU1="-gpu_id 1 -ntmpi 4 -ntomp 2 -nb gpu -bonded gpu -nmpe 1 -pme gpu -pmefft gpu -pin on -pinstride 0 -nstlist 100 -pinoffset 0"
+GPU0="-gpu_id 0 -ntmpi 4 -ntomp 2 -nb gpu -bonded gpu -npme 1 -pme gpu -pmefft gpu -pin on -pinstride 0 -nstlist 100 -pinoffset 49" 
+GPU1="-gpu_id 1 -ntmpi 4 -ntomp 2 -nb gpu -bonded gpu -npme 1 -pme gpu -pmefft gpu -pin on -pinstride 0 -nstlist 100 -pinoffset 0"
+export GMX_GPU_PME_PP_COMMS=true
+export GMX_FORCE_UPDATE_DEFAULT_GPU=1
+export GMX_GPU_DD_COMMS=true
 MDRUN_CPU="$GMX mdrun -nt $NT"
 MDRUN_GPU="$GMX mdrun $GPU0"
+
+
+	#Setup simulationtime
+if [ ! -z "$SIMULATIONTIME" ]
+then
+python_command=$(python <<EOF
+import re
+restep = re.compile("nsteps *= *(\d*)")
+redt = re.compile("dt *= *(\d*.\d*)")
+dt = 0
+simulationtime = float($SIMULATIONTIME) *1000 #Time in ps
+outputLines = []
+with open("mdp/md_prod.mdp",'r') as f:
+    mdp = f.readlines()
+    #find first the timestep
+    for line in mdp: 
+        dtmatch = redt.match(line)
+        if dtmatch:
+            dt = float(dtmatch.group(1))
+            break
+    for line in mdp:
+        stepmatch = restep.match(line)
+        if stepmatch and float(dt) > 0:
+            nsteps = int(simulationtime)/dt
+            line = "nsteps            = {}        ; {} * {} = {} ps or {} ns\n".format(int(nsteps),dt,nsteps, dt*nsteps, simulationtime/1000)
+        outputLines.append(line)
+    with open("mdp/md_prod.mdp",'w') as f:
+        for line in outputLines:
+            f.write(line)
+EOF
+)
+fi
+
+
 if [ ! -z "$LIGNAME" ]
 then
       #Create parameters directories
@@ -142,7 +180,7 @@ echo "SOL" | $GMX genion -s ions.tpr -o $PDB"_solv_ions.gro" -p topol.top -pname
 if [ ! -z "$LIGNAME" ]
 then
 #1. Add constraints to the ligand
-ndx=$($GMX make_ndx -f complex_solv_ions.gro -o ligand.ndx <<EOF
+    ndx=$($GMX make_ndx -f complex_solv_ions.gro -o index.ndx <<EOF
 1 | r LIG
 r SOL | r CL | r NA
 q
@@ -158,18 +196,21 @@ matches = re.findall(r'\[ \w+ \]', content)
 if matches:
     content = content.replace(matches[-1], '[ Water_Ions ]')
     content = content.replace(matches[-2], '[ Protein_Ligand ]')
-    with open('index2.ndx', 'w') as file:
+    with open('index.ndx', 'w') as file:
         file.write(content)
 EOF
 )
 
-#Now replace the groups in Equilibration and production mdp files
-sed -i 's/Protein Non-Protein/Protein_Ligand Water_Ions/g' mdp/nvt_300.mdp
-sed -i 's/Protein Non-Protein/Protein_Ligand Water_Ions/g' mdp/npt.mdp
-sed -i 's/Protein Non-Protein/Protein_Ligand Water_Ions/g' mdp/md_prod.mdp
+    #Now replace the groups in Equilibration and production mdp files
+    sed -i 's/Protein Non-Protein/Protein_Ligand Water_Ions/g' mdp/nvt_300.mdp
+    sed -i 's/Protein Non-Protein/Protein_Ligand Water_Ions/g' mdp/npt.mdp
+    sed -i 's/Protein Non-Protein/Protein_Ligand Water_Ions/g' mdp/md_prod.mdp
 
 
-INDEX="-n index.ndx"
+    INDEX="-n index.ndx"
+else
+INDEX=""
+
 fi
 
  
@@ -182,6 +223,7 @@ for ((i=0; i<$NUMBEROFREPLICAS; i++))
 	cp ../$PDB"_solv_ions.gro" .
 	cp ../topol.top .
 	cp ../*.itp .
+    cp ../index.ndx . 2> /dev/null
 
 
 	#######################
@@ -240,40 +282,9 @@ for ((i=0; i<$NUMBEROFREPLICAS; i++))
 	## Production 
 	#######################
 	
-	#Setup simulationtime
-if [ ! -z "$SIMULATIONTIME" ]
-then
-python_command=$(python <<EOF
-import re
-restep = re.compile("nsteps *= *(\d*)")
-redt = re.compile("dt *= *(\d*.\d*)")
-dt = 0
-simulationtime = float($SIMULATIONTIME) *1000 #Time in ps
-outputLines = []
-with open("mdp/md_prod.mdp",'r') as f:
-    mdp = f.readlines()
-    #find first the timestep
-    for line in mdp: 
-        dtmatch = redt.match(line)
-        if dtmatch:
-            dt = float(dtmatch.group(1))
-            break
-    for line in mdp:
-        stepmatch = restep.match(line)
-        if stepmatch and float(dt) > 0:
-            nsteps = int(simulationtime)/dt
-            line = "nsteps            = {}        ; {} * {} = {} ps or {} ns\n".format(int(nsteps),dt,nsteps, dt*nsteps, simulationtime/1000)
-        outputLines.append(line)
-    with open("mdp/md_prod.mdp",'w') as f:
-        for line in outputLines:
-            f.write(line)
-EOF
-)
 
-
-fi
 	
-	$GMX grompp -f mdp/md_prod.mdp -c results/npt/npt_ab.gro -t results/npt/npt_ab.cpt -p topol.top -o "md_"$PDB"_prod.tpr" -maxwarn 2
+	$GMX grompp -f mdp/md_prod.mdp -c results/npt/npt_ab.gro -t results/npt/npt_ab.cpt -p topol.top -o "md_"$PDB"_prod.tpr" -maxwarn 2 $INDEX
 	$MPI $MDRUN_GPU -deffnm "md_"$PDB"_prod"  -v
 
 	mkdir -p results/prod
