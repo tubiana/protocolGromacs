86a092964295d2208c92f0f9d04891bac7d82470
Thibault Tubiana <tubiana.thibault@gmail.com>
2024-03-14 13:05:45 +0100

    Fixing bug in configCPU.sh

--- configCPU.sh
+++ configCPU.sh
@@ -34,7 +34,7 @@ export GMX_GPU_PME_PP_COMMS=true
 export GMX_FORCE_UPDATE_DEFAULT_GPU=1
 export GMX_GPU_DD_COMMS=true
 MDRUN_CPU="srun $GMX mdrun"
-MDRUN_GPU="$GMX gmx mdrun -ntomp 2 -ntmpi 4 -npme 1 -bonded gpu -nb gpu -pme gpu -pmefft gpu"
+MDRUN_GPU="$GMX mdrun -ntomp 2 -ntmpi 4 -npme 1 -bonded gpu -nb gpu -pme gpu -pmefft gpu"
 MDRUN="$MDRUN_CPU"
 MDRUNmini=$MDRUN_CPU
 
